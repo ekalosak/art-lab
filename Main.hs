8ba7c9b028565a7eb210a91052030b6f4c0e219d
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2015-07-12 14:24:48 -0400

    Coppied a barebones skeleton Main.hs from Hackage

---
+++ Main.hs
@@ -0,0 +1,9 @@
+-- source: https://hackage.haskell.org/package/hnop-0.1/src/Main.hs
+-- Date sourced: 7/12/15 @ 14:23:04
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+module Main where
+
+-- | main, do nothing
+main :: IO ()
+main = return ()
